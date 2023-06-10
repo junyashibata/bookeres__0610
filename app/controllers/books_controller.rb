3116d42307fd566a1e9627c4e2ad074dd0486e6a
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-10 13:49:05 +0000

    make edit 4-kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,15 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+
   
   def create
     book = Book.new(list_params)
     book.save
-    redirect_to list_path(list.id)
+    redirect_to book_path(book.id)
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,9 +17,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  def list_params
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  private
+  
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
