a8822fbbd5ced4c645a73736ca11bccd9bcb0913
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-10 10:06:54 +0000

    remake 4-kadai

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to '/books/new'
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+end
