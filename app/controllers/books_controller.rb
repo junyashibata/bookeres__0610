25298a98d26dfe7031c9fb43f34324d89877e6c3
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-11 09:00:57 +0000

    flash 4-kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice]="Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -21,6 +22,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    flash[:notice]="Book was successfully updated."
     @book = Book.find(params[:id])
   end
   
